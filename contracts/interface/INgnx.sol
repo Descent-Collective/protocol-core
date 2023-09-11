f571bac20bbcff1334b3b33939171d39e7493b80
Njoku Emmanuel <kalunjoku123@gmail.com>
2023-09-11 02:01:07 +0100

    chore: add NGNX interface

---
+++ contracts/interface/INgnx.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+interface INGNX {
+    function mint(address account, uint amount) external returns (bool);
+
+    function burn(address account, uint amount) external returns (bool);
+
+    function permitToken(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 expiry,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+}
