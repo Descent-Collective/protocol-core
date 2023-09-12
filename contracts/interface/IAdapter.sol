b7fdf0930a643a3f2f77740ed652fbbbf13625e8
Njoku Emmanuel <kalunjoku123@gmail.com>
2023-09-12 10:29:50 +0100

    chore: add adapter interface

---
+++ contracts/interface/IAdapter.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+interface ICollateralAdapter {
+    function exit(uint256 amount, address owner, uint256 _vaultId) external;
+
+    function join(uint256 amount, address owner, uint256 _vaultId) external;
+}
+
+interface INGNXAdapter {
+    function exit(uint256 amount, address owner, uint256 _vaultId) external;
+
+    function join(uint256 amount, address owner, uint256 _vaultId) external;
+}
