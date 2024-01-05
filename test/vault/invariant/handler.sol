939f2484ec84200a901959a1f2966642bd78001d
Michael Amadi <amadimichaeld@gmail.com>
2024-01-05 20:18:22 +0100

    fix handler and base invariant

--- test/vault/invariant/handler.sol
+++ test/vault/invariant/handler.sol
@@ -1,13 +1,38 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.21;
 
-import {ERC20, IVault, Vault, StdInvariant} from "./baseInvariant.t.sol";
+import {ERC20, IVault, Vault} from "./baseInvariant.t.sol";
+import {Vm, StdCheats, StdUtils} from "forge-std/Test.sol";
 
-contract BaseInvariantTest {
+contract VaultHandler is StdCheats, StdUtils {
     Vault vault;
+    address[] internal actors;
+    address currentActor;
+
+    address internal constant VM_ADDRESS = address(uint160(uint256(keccak256("hevm cheat code"))));
+    Vm internal constant vm = Vm(VM_ADDRESS);
+
+    address user1 = vm.addr(uint256(keccak256("User1")));
+    address user2 = vm.addr(uint256(keccak256("User2")));
+    address user3 = vm.addr(uint256(keccak256("User3")));
+    address user4 = vm.addr(uint256(keccak256("User4")));
+    address user5 = vm.addr(uint256(keccak256("User5")));
 
     constructor(Vault _vault) {
         vault = _vault;
+
+        actors[0] = user1;
+        actors[1] = user2;
+        actors[2] = user3;
+        actors[3] = user4;
+        actors[4] = user5;
+    }
+
+    modifier useActor(uint256 actorIndexSeed) {
+        currentActor = actors[bound(actorIndexSeed, 0, actors.length - 1)];
+        vm.startPrank(currentActor);
+        _;
+        vm.stopPrank();
     }
 
     function depositCollateral() external {}
