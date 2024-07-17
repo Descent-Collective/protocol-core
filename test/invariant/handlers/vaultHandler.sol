47cad307320fc69e9d9c6e9f3a8ca6e55ebe3d85
Emmanuel Ozeh <ekemanuel.dev@gmail.com>
2024-07-17 19:01:49 +0100

    extract liquidatorHandler from vaultHandler in tests

--- test/invariant/handlers/vaultHandler.sol
+++ test/invariant/handlers/vaultHandler.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.21;
 
-import {Test, ERC20Token, IVault, Vault, console2, Currency, Liquidator} from "../../base.t.sol";
-import {ILiquidator} from "../../../src/interfaces/ILiquidator.sol";
+import {Test, ERC20Token, IVault, Vault, console2, Currency} from "../../base.t.sol";
 import {VaultGetters} from "../helpers/vaultGetters.sol";
 import {TimeManager} from "../helpers/timeManager.sol";
 
@@ -12,7 +11,6 @@ contract VaultHandler is Test {
     Vault vault;
     ERC20Token usdc;
     Currency xNGN;
-    Liquidator liquidatorContract;
     address owner = vm.addr(uint256(keccak256("OWNER")));
     address user1 = vm.addr(uint256(keccak256("User1")));
     address user2 = vm.addr(uint256(keccak256("User2")));
@@ -31,34 +29,19 @@ contract VaultHandler is Test {
     uint256 public totalMints;
     uint256 public totalBurns;
 
-    constructor(Vault _vault, ERC20Token _usdc, Currency _xNGN, VaultGetters _vaultGetters, TimeManager _timeManager, Liquidator _liquidatorContract) {
+    constructor(Vault _vault, ERC20Token _usdc, Currency _xNGN, VaultGetters _vaultGetters, TimeManager _timeManager) {
         timeManager = _timeManager;
 
         vault = _vault;
         usdc = _usdc;
         vaultGetters = _vaultGetters;
         xNGN = _xNGN;
-        liquidatorContract = _liquidatorContract;
 
         actors[0] = user1;
         actors[1] = user2;
         actors[2] = user3;
         actors[3] = user4;
         actors[4] = user5;
-
-        // FOR LIQUIDATIONS BY LIQUIDATOR
-        // mint usdc to address(this)
-        vm.startPrank(owner);
-        Currency(address(usdc)).mint(liquidator, 100_000_000_000 * (10 ** usdc.decimals()));
-        vm.stopPrank();
-
-        // use address(this) to deposit so that it can borrow currency needed for liquidation below
-        vm.startPrank(liquidator);
-        usdc.approve(address(vault), type(uint256).max);
-        vault.depositCollateral(usdc, liquidator, 100_000_000_000 * (10 ** usdc.decimals()));
-        vault.mintCurrency(usdc, liquidator, liquidator, 500_000_000_000e18);
-        xNGN.approve(address(vault), type(uint256).max);
-        vm.stopPrank();
     }
 
     modifier skipTime(uint256 skipTimeSeed) {
@@ -168,18 +151,6 @@ contract VaultHandler is Test {
         amount = bound(amount, 0, maxAmount);
         totalBurns += amount;
         vault.burnCurrency(usdc, currentOwner, amount);
-    }
-
-    function liquidate(uint256 skipTimeSeed, uint256 ownerIndexSeed)
-        external
-        skipTime(skipTimeSeed)
-        setOwner(ownerIndexSeed)
-    {
-        vm.startPrank(liquidator);
-
-        if (vaultGetters.getHealthFactor(vault, usdc, currentOwner)) vm.expectRevert(ILiquidator.PositionIsSafe.selector);
-        liquidatorContract.liquidate(vault, usdc, currentOwner, address(this), type(uint256).max);
-        vm.stopPrank();
     }
 
     function recoverToken(uint256 skipTimeSeed, bool isUsdc, address to) external skipTime(skipTimeSeed) {
